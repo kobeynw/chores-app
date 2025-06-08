80ff71324f4c6cf65a0a6dbf8865ce5eb4a3f8a1
Kobey Workman <kobeyworkman1@gmail.com>
2025-06-07 23:18:40 -0600

    Added mySQL configuration and created test ping for frontend to backend communication

---
+++ backend/app/controllers/api/v1/ping_controller.rb
@@ -0,0 +1,5 @@
+class Api::V1::PingController < ApplicationController
+  def index
+    render json: { message: "pong" }
+  end
+end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -7,4 +7,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  
+  namespace :api do
+    namespace :v1 do
+      get "/ping", to: "ping#index"
+    end
+  end
 end
