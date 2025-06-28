312e5849b6393249e4e283f93e5ee35c543f11e1
Kobey Workman <kobeyworkman1@gmail.com>
2025-06-28 16:47:45 -0600

    Updated User (Parent) model and created Child model; created seed file

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+  has_secure_password :passcode
+
+  has_many :children, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true
 end
